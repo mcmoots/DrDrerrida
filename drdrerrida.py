c1c9c8c439d1b7c368f40dd3929c73a4e408dc1f
mcmoots <criminy.crickets@gmail.com>
2014-06-22 23:03:56 -0700

    Refactored. Added sockets. New quotes.

--- drdrerrida.py
+++ drdrerrida.py
@@ -1,65 +1,101 @@
 # This script pulls random lyrics/quotes from two files
 # and mashes them together to make one <140 character tweet
 
-QUOTEF1 = 'dre_quotes.txt'
-QUOTEF2 = 'derrida_quotes.txt'
-
-TWEETSIZE = 140
-MAXTRIES = 500
-
 import random
+import sys
+from time import sleep
+import socket
+import logging
 import twitter
 import yaml
 
-# load api keys from config file
-twitter_tokens = yaml.load(open('config.yaml'))
-api = twitter.Api(**twitter_tokens)
 
-def parsequotes(fname):
-    "Output a dict of lines + char count for input file"
-    f = open(fname)
-    linecounts = [(line.strip(), len(line)) for line in f]
-    f.close()
-    return linecounts
+class Quotemasher:
+
+    def __init__(self, quotes1, quotes2):
+        """ Be sure to pass quotes1/quotes2 in a random order!
+
+        :param quotes1:
+        :param quotes2:
+        :return:
+        """
+        self.quotes1 = quotes1
+        self.quotes2 = quotes2
+
+    def buildTweet(self, tweetsize):
+        """ Pick lines from each quote list that sum to < tweetsize chars
+        :return: tweet text
+        """
+        try:
+            q1 = random.choice(filter(lambda x: len(x) < tweetsize - 1, self.quotes1))
+        except IndexError:
+            # nothing in quotes1 is shorter than tweetsize!
+            return None
+
+        try:
+            len2 = tweetsize - len(q1) - 2
+            q2 = random.choice(filter(lambda x: len(x) < len2, self.quotes2))
+        except IndexError:
+            # q1 is too long for any of the quotes in q2, that's ok, just tweet it alone
+            q2 = ''
+
+        if random.randrange(0, 2) > 0:
+            tweet = q1 + ' ' + q2
+        else:
+            tweet = q2 + ' ' + q1
+
+        #check for punctuation at the end, add some if none
+        if tweet.endswith(('?', ',', '.', '!')):
+            pass
+        else:
+            tweet += '.'
+
+        return tweet
 
 
-def assembletweet(qf1, qf2):
-    "Pick lines from each of q1 & q2 that sum to < TWEETSIZE chars"
-    order = random.randrange(0, 2)
-    if order > 0:
-        q1 = parsequotes(qf1)
-        q2 = parsequotes(qf2)
+def run_drdrerrida(rootdir):
+
+    drefile = rootdir + 'dre_quotes.txt'
+    derridafile = rootdir + 'derrida_quotes.txt'
+
+    with open(drefile) as f:
+        drequotes = [line.strip() for line in f]
+    with open(derridafile) as f:
+        derridaquotes = [line.strip() for line in f]
+
+    twitter_tokens = yaml.load(open(rootdir + 'config.yaml'))
+    api = twitter.Api(**twitter_tokens)
+
+    while True:
+        if random.randrange(0, 2) > 0:
+            q = Quotemasher(drequotes, derridaquotes)
+        else:
+            q = Quotemasher(derridaquotes, drequotes)
+
+        tweet = q.buildTweet(140)
+
+        try:
+            api.PostUpdate(tweet)
+        except twitter.TwitterError:
+            pass
+
+        sleep(random.randrange(6666, 9999))
+
+
+if __name__ == '__main__':
+    lock_socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+    lock_id = "mcmoots.drdrerrida"
+    try:
+        lock_socket.bind('\0' + lock_id)
+        logging.debug("Got lock %r" % (lock_id,))
+    except socket.error:
+        # socket already locked, task must be running
+        logging.info("Failed to get lock %r" % (lock_id,))
+        sys.exit()
+
+    if len(sys.argv) > 1:
+        root = sys.argv[1]
     else:
-        q2 = parsequotes(qf1)
-        q1 = parsequotes(qf2)
-    # pick random quote from f1
-    i = 0
-    min2 = min(q2, key=lambda x: x[1])[1]
-    for i in range(MAXTRIES):
-        r1 = random.randrange(0, len(q1))
-        if TWEETSIZE - q1[r1][1] - 1 > min2:
-            break
-    else:
-        return None  # write some exception handling if loop can't pick short q1
-    for i in range(MAXTRIES):
-        r2 = random.randrange(0, len(q2))
-        if TWEETSIZE - q1[r1][1] - 1 - q2[r2][1] >= 0:
-            break
-    else:
-        return None  # more exception handling if loop can't pick short q2
-    # Uppercase beginning of each quote
-    t1 = q1[r1][0][0].upper() + q1[r1][0][1:len(q1[r1][0])]
-    t2 = q2[r2][0][0].upper() + q2[r2][0][1:len(q2[r2][0])]
-    # Check for punctuation at the end, add some if none.
-    if t1.endswith(('?', ',', '.', '!')):
-        pass
-    else:
-        t1 = t1 + '.'
+        root = './'
 
-    tweet = t1 + ' ' + t2
-    return tweet
-
-#Run the bot!
-tweet = assembletweet(QUOTEF1, QUOTEF2)
-if len(tweet) > 0:
-    api.PostUpdates(tweet)
+    run_drdrerrida(root)
